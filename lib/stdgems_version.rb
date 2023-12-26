68901a88a04d4752ebcd6bd3c5f1ea1c17b3e309
Taichi Ishitani <taichi730@gmail.com>
2023-12-27 00:53:13 +0900

    implement StdgemsVersion.version method

--- lib/stdgems_version.rb
+++ lib/stdgems_version.rb
@@ -1,3 +1,59 @@
 # frozen_string_literal: true
 
+require 'json'
 require_relative 'stdgems_version/version'
+
+module StdgemsVersion
+  class << self
+    def version(gem)
+      matched_version = find_version(gem, bundled_gems)
+      return matched_version if matched_version
+
+      find_version(gem, default_gems)
+    end
+
+    attr_writer :ruby_version
+
+    private
+
+    def find_version(gem, gem_list)
+      entry = gem_list['gems'].find { |e| e['gem'] == gem.to_s }
+      return unless entry
+
+      entry['versions'].inject(nil) do |matched_version, (ruby_version, gem_version)|
+        if match_ruby_version?(ruby_version)
+          gem_version
+        else
+          matched_version
+        end
+      end
+    end
+
+    def match_ruby_version?(version)
+      if version.count('.') == 1
+        Gem::Requirement.create("~> #{version}.0").satisfied_by?(ruby_version)
+      else
+        Gem::Requirement.create(version).satisfied_by?(ruby_version)
+      end
+    end
+
+    def ruby_version
+      @ruby_version ||= Gem::Version.create(RUBY_VERSION)
+    end
+
+    def bundled_gems
+      @bundled_gems ||= load_json('bundled_gems.json')
+    end
+
+    def default_gems
+      @default_gems ||= load_json('default_gems.json')
+    end
+
+    STDGEMS_DIR = File.expand_path('../stdgems', __dir__)
+
+    def load_json(file)
+      json = File.read(File.join(STDGEMS_DIR, file))
+      JSON.parse(json)
+    end
+  end
+end
--- spec/stdgems_version_spec.rb
+++ spec/stdgems_version_spec.rb
@@ -4,4 +4,135 @@ RSpec.describe StdgemsVersion do
   it 'has a version number' do
     expect(StdgemsVersion::VERSION).not_to be nil
   end
+
+  describe '.version' do
+    it 'should return version of the given default/bundled gem installed on the executing ruby' do
+      stub_const('RUBY_VERSION', '3.3.0')
+      expect(StdgemsVersion.version('csv')).to eq '3.2.8'
+      expect(StdgemsVersion.version('debug')).to eq '1.9.0'
+      expect(StdgemsVersion.version('did_you_mean')).to eq '1.6.3'
+      expect(StdgemsVersion.version('prism')).to eq '0.19.0'
+      expect(StdgemsVersion.version('rexml')).to eq '3.2.6'
+
+      StdgemsVersion.ruby_version = nil
+      stub_const('RUBY_VERSION', '3.2.2')
+      expect(StdgemsVersion.version('csv')).to eq '3.2.6'
+      expect(StdgemsVersion.version('debug')).to eq '1.7.1'
+      expect(StdgemsVersion.version('did_you_mean')).to eq '1.6.3'
+      expect(StdgemsVersion.version('prism')).to be_nil
+      expect(StdgemsVersion.version('rexml')).to eq '3.2.5'
+
+      StdgemsVersion.ruby_version = nil
+      stub_const('RUBY_VERSION', '3.2.1')
+      expect(StdgemsVersion.version('csv')).to eq '3.2.6'
+      expect(StdgemsVersion.version('debug')).to eq '1.7.1'
+      expect(StdgemsVersion.version('did_you_mean')).to eq '1.6.3'
+      expect(StdgemsVersion.version('prism')).to be_nil
+      expect(StdgemsVersion.version('rexml')).to eq '3.2.5'
+
+      StdgemsVersion.ruby_version = nil
+      stub_const('RUBY_VERSION', '3.2.0')
+      expect(StdgemsVersion.version('csv')).to eq '3.2.6'
+      expect(StdgemsVersion.version('debug')).to eq '1.7.1'
+      expect(StdgemsVersion.version('did_you_mean')).to eq '1.6.3'
+      expect(StdgemsVersion.version('prism')).to be_nil
+      expect(StdgemsVersion.version('rexml')).to eq '3.2.5'
+
+      StdgemsVersion.ruby_version = nil
+      stub_const('RUBY_VERSION', '3.1.4')
+      expect(StdgemsVersion.version('csv')).to eq '3.2.5'
+      expect(StdgemsVersion.version('debug')).to eq '1.6.3'
+      expect(StdgemsVersion.version('did_you_mean')).to eq '1.6.1'
+      expect(StdgemsVersion.version('prism')).to be_nil
+      expect(StdgemsVersion.version('rexml')).to eq '3.2.5'
+
+      StdgemsVersion.ruby_version = nil
+      stub_const('RUBY_VERSION', '3.1.3')
+      expect(StdgemsVersion.version('csv')).to eq '3.2.5'
+      expect(StdgemsVersion.version('debug')).to eq '1.6.3'
+      expect(StdgemsVersion.version('did_you_mean')).to eq '1.6.1'
+      expect(StdgemsVersion.version('prism')).to be_nil
+      expect(StdgemsVersion.version('rexml')).to eq '3.2.5'
+
+      StdgemsVersion.ruby_version = nil
+      stub_const('RUBY_VERSION', '3.1.2')
+      expect(StdgemsVersion.version('csv')).to eq '3.2.2'
+      expect(StdgemsVersion.version('debug')).to eq '1.4.0'
+      expect(StdgemsVersion.version('did_you_mean')).to eq '1.6.1'
+      expect(StdgemsVersion.version('prism')).to be_nil
+      expect(StdgemsVersion.version('rexml')).to eq '3.2.5'
+
+      StdgemsVersion.ruby_version = nil
+      stub_const('RUBY_VERSION', '3.1.1')
+      expect(StdgemsVersion.version('csv')).to eq '3.2.2'
+      expect(StdgemsVersion.version('debug')).to eq '1.4.0'
+      expect(StdgemsVersion.version('did_you_mean')).to eq '1.6.1'
+      expect(StdgemsVersion.version('prism')).to be_nil
+      expect(StdgemsVersion.version('rexml')).to eq '3.2.5'
+
+      StdgemsVersion.ruby_version = nil
+      stub_const('RUBY_VERSION', '3.1.0')
+      expect(StdgemsVersion.version('csv')).to eq '3.2.2'
+      expect(StdgemsVersion.version('debug')).to eq '1.4.0'
+      expect(StdgemsVersion.version('did_you_mean')).to eq '1.6.1'
+      expect(StdgemsVersion.version('prism')).to be_nil
+      expect(StdgemsVersion.version('rexml')).to eq '3.2.5'
+
+      StdgemsVersion.ruby_version = nil
+      stub_const('RUBY_VERSION', '3.0.6')
+      expect(StdgemsVersion.version('csv')).to eq '3.1.9'
+      expect(StdgemsVersion.version('debug')).to eq '0.2.1'
+      expect(StdgemsVersion.version('did_you_mean')).to eq '1.5.0'
+      expect(StdgemsVersion.version('prism')).to be_nil
+      expect(StdgemsVersion.version('rexml')).to eq '3.2.5'
+
+      StdgemsVersion.ruby_version = nil
+      stub_const('RUBY_VERSION', '3.0.5')
+      expect(StdgemsVersion.version('csv')).to eq '3.1.9'
+      expect(StdgemsVersion.version('debug')).to eq '0.2.1'
+      expect(StdgemsVersion.version('did_you_mean')).to eq '1.5.0'
+      expect(StdgemsVersion.version('prism')).to be_nil
+      expect(StdgemsVersion.version('rexml')).to eq '3.2.5'
+
+      StdgemsVersion.ruby_version = nil
+      stub_const('RUBY_VERSION', '3.0.4')
+      expect(StdgemsVersion.version('csv')).to eq '3.1.9'
+      expect(StdgemsVersion.version('debug')).to eq '0.2.1'
+      expect(StdgemsVersion.version('did_you_mean')).to eq '1.5.0'
+      expect(StdgemsVersion.version('prism')).to be_nil
+      expect(StdgemsVersion.version('rexml')).to eq '3.2.5'
+
+      StdgemsVersion.ruby_version = nil
+      stub_const('RUBY_VERSION', '3.0.3')
+      expect(StdgemsVersion.version('csv')).to eq '3.1.9'
+      expect(StdgemsVersion.version('debug')).to eq '0.2.1'
+      expect(StdgemsVersion.version('did_you_mean')).to eq '1.5.0'
+      expect(StdgemsVersion.version('prism')).to be_nil
+      expect(StdgemsVersion.version('rexml')).to eq '3.2.5'
+
+      StdgemsVersion.ruby_version = nil
+      stub_const('RUBY_VERSION', '3.0.2')
+      expect(StdgemsVersion.version('csv')).to eq '3.1.9'
+      expect(StdgemsVersion.version('debug')).to eq '0.1.0'
+      expect(StdgemsVersion.version('did_you_mean')).to eq '1.5.0'
+      expect(StdgemsVersion.version('prism')).to be_nil
+      expect(StdgemsVersion.version('rexml')).to eq '3.2.5'
+
+      StdgemsVersion.ruby_version = nil
+      stub_const('RUBY_VERSION', '3.0.1')
+      expect(StdgemsVersion.version('csv')).to eq '3.1.9'
+      expect(StdgemsVersion.version('debug')).to eq '0.1.0'
+      expect(StdgemsVersion.version('did_you_mean')).to eq '1.5.0'
+      expect(StdgemsVersion.version('prism')).to be_nil
+      expect(StdgemsVersion.version('rexml')).to eq '3.2.5'
+
+      StdgemsVersion.ruby_version = nil
+      stub_const('RUBY_VERSION', '3.0.0')
+      expect(StdgemsVersion.version('csv')).to eq '3.1.9'
+      expect(StdgemsVersion.version('debug')).to eq '0.1.0'
+      expect(StdgemsVersion.version('did_you_mean')).to eq '1.5.0'
+      expect(StdgemsVersion.version('prism')).to be_nil
+      expect(StdgemsVersion.version('rexml')).to eq '3.2.4'
+    end
+  end
 end
